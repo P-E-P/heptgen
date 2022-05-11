3628693d748257d5301e835d49fa68e71448bb1b
Pierre-Emmanuel Patry <pierre-emmanuel.patry@lse.epita.fr>
2022-05-11 21:01:11 +0200

    feat: Add file extension verification
    Add file  extension verification as well as a CLI option to force
    the parsing of a file with an incorrect extension. This  mecanism
    will hopefully prevent the generation of files against the wrong
    code file.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
-use clap::{arg, command};
+use clap::{arg, command, ArgMatches};
+use std::path::Path;
+
+const HEPTAGON_INTERFACE_EXTENSION: &str = "epi";
 
 fn main() {
     let matches = command!()
-        .arg(
-            arg!([FILE])
-        ).get_matches();
+        .arg(arg!([FILE]))
+        .arg(arg!(-f - -force "Force file parsing"))
+        .get_matches();
 
-    println!("File is: {}", matches.value_of("FILE").expect("Missing file name"));
+    let filepath = match matches.value_of("FILE") {
+        Some(filename) => Path::new(filename),
+        None => panic!("No file specified"),
+    };
+
+    if !validate_file_extension(filepath, &matches) {
+        panic!("Invalid file extension");
+    }
+}
+
+fn validate_file_extension(filename: &Path, matches: &ArgMatches) -> bool {
+    filename
+        .extension()
+        .map(|s| s == HEPTAGON_INTERFACE_EXTENSION)
+        .unwrap_or(false)
+        || matches.is_present("force")
 }
